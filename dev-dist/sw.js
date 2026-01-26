57f826a929d469622bb51a8399829b1d7bd37069
root <info.aifunschool@gmail.com>
2026-01-26 14:53:49 +0800

    安全性更新

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-bf3571c7'], (function (workbox) { 'use strict';
     "revision": "0069240704f8ea44c26cfc342a71c899"
   }, {
     "url": "/brain-training/index.html",
-    "revision": "0.tlbe4lspo7"
+    "revision": "0.hkinc9ejch8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/brain-training/index.html"), {
